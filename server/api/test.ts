191cb2aa015617d5d04fa2a2a7935d297676b2cc
czyan <czyang2002@gmail,com>
2022-10-03 12:09:46 +0800

    added prisma connection

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,3 +1,9 @@
-export default defineEventHandler((event) => {
-  return 'Hello test'
-})
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const user = await prisma.user.findMany();
+
+  return user;
+});
